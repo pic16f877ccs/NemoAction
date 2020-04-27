e3592a86aae8b0076d9b4b9b035f23b7077033a8
Karl Wulfert <pic16f877.ccs@emailn.de>
2020-04-27 18:05:16 +0200

    Remove folder mv

--- Nemo-action/file-copy-rename/file-copy-rename.sh
+++ Nemo-action/file-copy-rename/file-copy-rename.sh
@@ -40,41 +40,38 @@ base_name_rename=$(zenity --entry --title="$title" --text="$text" --entry-text="
 #
 # Erstelle ein Verzeichnis, wenn es noch nicht existiert
 if [ ! -e "$dir_name/$base_name_rename" ] 
-	then mkdir "$dir_name/$base_name_rename" ;
+	then mkdir "$dir_name/../$base_name_rename" ;
 fi ;
-#
-#
-mv -i "$dir_name/$base_name_rename" "$dir_name/$base_name_rename/../.."
 #  
 # Test rename and copy files.
-cp -i "$dir_name/${base_name_cut}.pro" "$dir_name/${base_name_rename}.pro" ;
+cp -i "$dir_name/${base_name_cut}.pro" "$dir_name/../$base_name_rename/${base_name_rename}.pro" ;
 #
 if [ -f "$dir_name/${base_name_cut}.sch" ] 
-    then cp -i "$dir_name/${base_name_cut}.sch" "$dir_name/${base_name_rename}.sch" ;   
+    then cp -i "$dir_name/${base_name_cut}.sch" "$dir_name/../$base_name_rename/${base_name_rename}.sch" ;   
 fi ;
 #
 if [ -f "$dir_name/${base_name_cut}.kicad_pcb" ] 
-    then cp -i "$dir_name/${base_name_cut}.kicad_pcb" "$dir_name/${base_name_rename}.kicad_pcb" ;   
+    then cp -i "$dir_name/${base_name_cut}.kicad_pcb" "$dir_name/../$base_name_rename/${base_name_rename}.kicad_pcb" ;   
 fi ;
 #
 if [ -f "$dir_name/${base_name_cut}.net" ] 
-    then cp -i "$dir_name/${base_name_cut}.net" "$dir_name/${base_name_rename}.net" ;   
+    then cp -i "$dir_name/${base_name_cut}.net" "$dir_name/../$base_name_rename/${base_name_rename}.net" ;   
 fi ;
 #
 if [ -f "$dir_name/${base_name_cut}-cache.lib" ] 
-    then cp -i "$dir_name/${base_name_cut}-cache.lib" "$dir_name/${base_name_rename}-cache.lib" ;   
+    then cp -i "$dir_name/${base_name_cut}-cache.lib" "$dir_name/../$base_name_rename/${base_name_rename}-cache.lib" ;   
 fi ;
 #
 if [ -f "$dir_name/${base_name_cut}.kicad_wks" ] 
-    then cp -i "$dir_name/${base_name_cut}.kicad_wks" "$dir_name/${base_name_rename}.kicad_wks" ;   
+    then cp -i "$dir_name/${base_name_cut}.kicad_wks" "$dir_name/../$base_name_rename/${base_name_rename}.kicad_wks" ;   
 fi ;
 #
 if [ -f "$dir_name/${base_name_cut}.kicad_mod" ] 
-    then cp -i "$dir_name/${base_name_cut}.kicad_mod" "$dir_name/${base_name_rename}.kicad_mod" ;   
+    then cp -i "$dir_name/${base_name_cut}.kicad_mod" "$dir_name/../$base_name_rename/${base_name_rename}.kicad_mod" ;   
 fi ;
 #
 if [ -f "$dir_name/fp-info-cache" ] 
-    then cp -i "$dir_name/fp-info-cache" "$dir_name/fp-info-cache" ;   
+    then cp -i "$dir_name/fp-info-cache" "$dir_name/../$base_name_rename/fp-info-cache" ;   
 fi ;
 #  
 #
